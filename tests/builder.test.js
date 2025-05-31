083b0d311a6f84d2cbcf35b5fbe65dcdecca9a7b
Michael Wieland <git@selfcoders.com>
2025-05-31 15:55:59 +0200

    Add tests for building query

---
+++ tests/builder.test.js
@@ -0,0 +1,18 @@
+import { describe, it, expect } from "vitest";
+import { SearchQueryParser, Filter } from "../src/search-query-parser";
+
+describe("Build query", () => {
+    it("Simple query", () => {
+        let queryParser = new SearchQueryParser();
+
+        queryParser.rootGroup.add(new Filter(null, "hello", ":"));
+
+        expect(queryParser.toString()).toEqual("hello");
+        expect(queryParser.matchesEntry({somefield: "hello from me"})).toBeTruthy();
+
+        queryParser.rootGroup.add(new Filter("somefield", "hello world", "="));
+
+        expect(queryParser.toString()).toEqual('hello somefield="hello world"');
+        expect(queryParser.matchesEntry({somefield: "Hello World", anotherfield: "hello from me"})).toBeTruthy();
+    });
+});
